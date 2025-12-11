ac71a9331d3a9a62352ef69c0bea38cf63d01e86
barthelemy pousset <barthelemypousset@gmail.com>
2025-12-11 15:03:51 +0100

    route get chapters + superTest

---
+++ tests/chapter.test.js
@@ -0,0 +1,41 @@
+const request = require("supertest");
+const app = require("../app");
+const mongoose = require("mongoose");
+
+test("GET /chapters should return all chapters", async () => {
+  const res = await request(app).get("/chapter");
+
+  expect(res.status).toBe(200);
+  expect(res.body.result).toBe(true);
+
+  expect(res.body.chapters.length).toBeGreaterThan(1);
+  expect(res.body.chapters[1].nbChapter).toBe(2);
+});
+
+test("GET /chapters/:id should return a single chapter", async () => {
+  const res = await request(app).get(`/chapter/2`);
+
+  expect(res.status).toBe(200);
+  expect(res.body.result).toBe(true);
+
+  expect(res.body.chapter.titleChapter).toBe("Chapitre 2: Le lâcher-prise");
+});
+
+test("GET /chapters/:id should return 404 if not found", async () => {
+  const id = new mongoose.Types.ObjectId();
+  const res = await request(app).get(`/chapter/404`);
+
+  expect(res.status).toBe(404);
+  expect(res.body.result).toBe(false);
+});
+
+test("GET /chapters/:id should return 400 for invalid ID", async () => {
+  const res = await request(app).get("/chapter/invalidID");
+
+  expect(res.status).toBe(400);
+  expect(res.body.result).toBe(false);
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+});
