55d7e9c01333917aca36c32002cf8db3ca16f3ab
barthelemy pousset <barthelemypousset@gmail.com>
2025-12-12 18:27:50 +0100

    update tests

--- tests/chapter.test.js
+++ tests/chapter.test.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
 const request = require("supertest");
 const app = require("../app");
 const mongoose = require("mongoose");
 
 test("GET /chapters should return all chapters", async () => {
-  const res = await request(app).get("/chapter");
+  const res = await request(app).get("/chapters");
 
   expect(res.status).toBe(200);
   expect(res.body.result).toBe(true);
@@ -13,24 +14,24 @@ test("GET /chapters should return all chapters", async () => {
 });
 
 test("GET /chapters/:id should return a single chapter", async () => {
-  const res = await request(app).get(`/chapter/2`);
+  const res = await request(app).get(`/chapters/2`);
 
   expect(res.status).toBe(200);
   expect(res.body.result).toBe(true);
 
-  expect(res.body.chapter.title).toBe("Chapitre 2: Le lâcher-prise");
+  expect(res.body.chapters.title).toBe("Chapitre 2: Le lâcher-prise");
 });
 
 test("GET /chapters/:id should return 404 if not found", async () => {
   const id = new mongoose.Types.ObjectId();
-  const res = await request(app).get(`/chapter/404`);
+  const res = await request(app).get(`/chapters/404`);
 
   expect(res.status).toBe(404);
   expect(res.body.result).toBe(false);
 });
 
 test("GET /chapters/:id should return 400 for invalid ID", async () => {
-  const res = await request(app).get("/chapter/invalidID");
+  const res = await request(app).get("/chapters/invalidID");
 
   expect(res.status).toBe(400);
   expect(res.body.result).toBe(false);
