aa0b14de97cc309e19354d22b468e87f3950529c
PffGit <pff@duck.com>
2025-12-14 23:06:09 +0100

    oubli ligne require const User dans seed.js résolu

--- scripts/seed.js
+++ scripts/seed.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const fs = require('fs');
 const Chapter = require('../models/chapters');
+const User = require('../models/users');
 require('dotenv').config();
 
 // Lire le fichier JSON
